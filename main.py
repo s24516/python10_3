55ebb64e38142ba2bb6a2ef4ed61730ec45e4726
s24516 <s24516@pjwstk.edu.pl>
2023-03-10 19:36:15 +0100

    zmiany

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 import webbrowser
-pageUrl = 'https://github.com'
-webbrowser.open(pageUrl)
+import requests
+
+pageurl = input("podaj adres strony: ")
+date=input("podaj date: ")
+url = "http://archive.org/wayback/available?url=" + pageurl + "&timestamp=" + str(date)
+response = requests.get(url)
+d = response.json()
+print(d)
+page = d["archived_snapshots"]["closest"]["url"]
+webbrowser.open(pageurl)
+
+pageurl = input("podaj adres strony: ")
+date=input("podaj date: ")
+url = "http://archive.org/wayback/available?url=" + pageurl + "&timestamp=" + str(date)
+response = requests.get(url)
+d = response.json()
+print(d)
+page = d["archived_snapshots"]["closest"]["url"]
+webbrowser.open(pageurl)
+
+pageurl = input("podaj adres strony: ")
+date=input("podaj date: ")
+url = "http://archive.org/wayback/available?url=" + pageurl + "&timestamp=" + str(date)
+response = requests.get(url)
+d = response.json()
+print(d)
+page = d["archived_snapshots"]["closest"]["url"]
+webbrowser.open(pageurl)
+
+
+
+
